acc5ce8f09f0960edae3493179fc6d6ad03f033f
Andrew Melton <andrew.melton@rackspace.com>
2015-03-13 19:17:13 +0000

    Add bay_uuid to container

--- magnumclient/v1/containers.py
+++ magnumclient/v1/containers.py
@@ -19,7 +19,7 @@ from magnumclient.common import utils
 from magnumclient import exceptions
 
 
-CREATION_ATTRIBUTES = ['name', 'image_id']
+CREATION_ATTRIBUTES = ['name', 'image_id', 'bay_uuid']
 
 
 class Container(base.Resource):
